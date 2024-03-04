197ccac4b708ae00e164e3663116ec471b57f377
Saikrishna <saikrishnabudi@outlook.com>
2024-03-04 14:04:56 +0530

    folder structure added

---
+++ main.py
@@ -0,0 +1,4 @@
+from textsummarization.logging import logger
+
+logger.info("Welcome to our custom logging")
+
---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as f:
+    long_description = f.read()
+
+
+
+__version__ = "0.0.0"
+
+REPO_NAME =  "NLP-Text-Summarization-Project"
+AUTHOR_USER_NAME= "saikrishnabudi"
+SRC_REPO= "textsummarization"
+AUTHOR_EMAIL= "saikrishnabudi786@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Text Summarization Project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/saikrishnabudi/NLP-Text-Summarization-Project",
+    project_urls={
+        "Bug Tracker": "https://github.com/saikrishnabudi/NLP-Text-Summarization-Project/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+   
+)
